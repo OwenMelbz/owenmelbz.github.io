078aa911c83a89095b3d0b7aea532f54b11bc01a
OwenMelbz <owenmelbz@gmail.com>
2015-11-21 15:50:08 +0000

    more tweaks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt){
                 files : [{
                     expand: true,
                     cwd: 'images/',
-                    src: '**/*.{png,jpg,gif,svg}',
+                    src: '**/*.{png,jpg,gif}',
                     dest: 'images/'
                 }]
             }
