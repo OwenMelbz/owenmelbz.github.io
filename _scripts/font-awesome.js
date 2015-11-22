b4ada37468e8f2e34e68622b683867f79976c126
OwenMelbz <owenmelbz@gmail.com>
2015-11-22 09:39:41 +0000

    optimised some css loading

---
+++ _scripts/font-awesome.js
@@ -0,0 +1,6 @@
+$(window).load(function(){
+
+    var library = '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css';
+    $('<link rel="stylesheet" type="text/css" href="' + library + '" >').appendTo("head");
+
+});
