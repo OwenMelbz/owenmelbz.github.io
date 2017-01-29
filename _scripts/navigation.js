8dc50591e1fa18c0324cf7e3b87afd1cac0d4511
Owen <owen@selesti.com>
2017-01-29 09:51:45 +0000

    2017 tweaks

--- _scripts/navigation.js
+++ _scripts/navigation.js
@@ -1,6 +1,12 @@
 $(function(){
     $('#services-link').on('click', function(evt){
         evt.preventDefault();
+
+        $('#sidebar').addClass('skip-featured');
         $('.services-and-freelance').trigger('click');
+
+        setTimeout(function(){
+            $('#sidebar').removeClass('skip-featured');
+        }, 2000);
     });
 });
